867e1dcf2b95a8fedd4a38f52791c49fd1e81021
student <student@lab-45-14.cs.put.poznan.pl>
2019-11-04 17:30:56 +0100

    C

--- Program.java
+++ Program.java
@@ -4,6 +4,18 @@ public class MyClass {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
+		System.out.println("Test2");
+	}
+	
+	public static void A() {
+		System.out.println("A");
+	}
+	
+	public static void B() {
+		System.out.println("B");
+	}
+	
+	public static void C() {
+		System.out.println("C");
 	}
 }
